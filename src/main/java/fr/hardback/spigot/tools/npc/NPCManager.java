f725291916dba4613c1321f0dd8a24bd032e59b3
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-04 21:02:19 +0200

    :cyclone: NPCManager create and remove methods created

--- src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
+++ src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
@@ -3,49 +3,63 @@ package fr.hardback.spigot.tools.npc;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 
-import fr.hardback.spigot.api.HardBackAPI;
 import net.minecraft.server.v1_8_R3.*;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
-import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
 
 import java.util.UUID;
 
 public class NPCManager {
 
-    public void createNPC(Location location, String name, String texture, String signature, Player player){
-        MinecraftServer nmsServer = ((CraftServer) Bukkit.getServer()).getServer();
-        WorldServer nsmWorld = ((CraftWorld) location.getWorld()).getHandle();
-        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), name);
+    public void createNPC(NPC npc, Plugin plugin){
+        final MinecraftServer minecraftServer = ((CraftServer) Bukkit.getServer()).getServer();
+        final GameProfile gameProfile = new GameProfile(UUID.randomUUID(), npc.getName());
 
-        changeSKin(gameProfile, texture, signature);
+        final NPCSkin npcSkin = new NPCSkin();
+        final String[] skin = npcSkin.getFromPlayer(Bukkit.getPlayer(npc.getSkinName()));
+        gameProfile.getProperties().put("textures", new Property("textures", skin[0], skin[1]));
 
-        EntityPlayer npc = new EntityPlayer(nmsServer, nsmWorld, gameProfile, new PlayerInteractManager(nsmWorld));
-        npc.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        final EntityPlayer entityPlayer = new EntityPlayer(minecraftServer, minecraftServer.getWorldServer(0), gameProfile, new PlayerInteractManager(minecraftServer.getWorldServer(0)));
+        entityPlayer.setPosition(npc.getX(), npc.getY(), npc.getZ());
 
-        PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
-        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, npc));
-        connection.sendPacket(new PacketPlayOutNamedEntitySpawn(npc));
+        final Location location = new Location(Bukkit.getWorld("world"), npc.getX(), npc.getY(), npc.getZ() - 0.25);
+        final Vector direction = location.getDirection();
+        final Location front = location.add(direction);
+        final ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(front, EntityType.ARMOR_STAND);
+        armorStand.setCustomName(npc.getName());
+        armorStand.setCustomNameVisible(false);
+        armorStand.setVisible(false);
+        armorStand.setGravity(false);
 
-        Player npcPlayer = npc.getBukkitEntity().getPlayer();
-        CraftPlayer cp = (CraftPlayer) npcPlayer;
+        Bukkit.getOnlinePlayers().forEach(players -> {
+            ((CraftPlayer) players).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entityPlayer));
+            ((CraftPlayer) players).getHandle().playerConnection.sendPacket(new PacketPlayOutNamedEntitySpawn(entityPlayer));
 
-        Bukkit.getScheduler().runTaskLater(HardBackAPI.get().getPlugin(), () -> {
-            sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, cp.getHandle()));
-        }, 20 * 3);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> ((CraftPlayer) players).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, entityPlayer)), 5);
+        });
     }
 
-    private void changeSKin(GameProfile gameProfile, String texture, String signature){
-        gameProfile.getProperties().put("texture", new Property("textures", texture, signature));
-    }
+    public static void removeNPC(NPC npc){
+        final MinecraftServer minecraftServer = ((CraftServer) Bukkit.getServer()).getServer();
+        final GameProfile gameProfile = new GameProfile(UUID.randomUUID(), npc.getName());
+        final EntityPlayer entityPlayer = new EntityPlayer(minecraftServer, minecraftServer.getWorldServer(0), gameProfile, new PlayerInteractManager(minecraftServer.getWorldServer(0)));
 
-    private static void sendPacket (Packet< ? > packet){
-        for (Player all : Bukkit.getOnlinePlayers()) {
-            ((CraftPlayer) all).getHandle().playerConnection.sendPacket(packet);
-        }
+        Bukkit.getOnlinePlayers().forEach(players -> {
+            ((CraftPlayer) players).getHandle().playerConnection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, entityPlayer));
+            ((CraftPlayer) players).getHandle().playerConnection.sendPacket(new PacketPlayOutEntityDestroy(entityPlayer.getId()));
+        });
+
+        Bukkit.getWorlds().forEach(worlds -> worlds.getEntities().forEach(entities -> {
+           if(entities instanceof ArmorStand & entities.getCustomName().equalsIgnoreCase(npc.getName())){
+               entities.remove();
+           }
+        }));
     }
 }
