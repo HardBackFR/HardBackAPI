f997a3cad5b41df7258be2af297b3350f8f3853e
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-11 21:11:59 +0200

    :cyclone: direction

--- src/main/java/fr/hardback/spigot/tools/DirectionUtils.java
+++ src/main/java/fr/hardback/spigot/tools/DirectionUtils.java
@@ -20,7 +20,7 @@ import org.bukkit.entity.Player;
  */
 public class DirectionUtils
 {
-    public enum Directions { NORTH, SOUTH, EAST, WEST }
+    public enum Directions { NORTH, NORTH_WEST, NORTH_EAST, SOUTH, SOUTH_WEST, SOUTH_EAST, EAST, WEST }
 
     /**
      * Get given player's location with his location
@@ -38,12 +38,18 @@ public class DirectionUtils
 
         if (0 <= rotation && rotation < 22.5)
             return Directions.NORTH;
+        else if(22.5 <= rotation && rotation  < 67.5)
+            return Directions.NORTH_EAST;
         else if (67.5 <= rotation && rotation < 112.5)
             return Directions.EAST;
         else if (157.5 <= rotation && rotation < 202.5)
             return Directions.SOUTH;
+        else if(202.5 <= rotation && rotation < 247.5)
+            return Directions.SOUTH_WEST;
         else if (247.5 <= rotation && rotation < 292.5)
             return Directions.WEST;
+        else if (292.5 <= rotation && rotation < 337.5)
+            return Directions.NORTH_WEST;
         else if (337.5 <= rotation && rotation < 360.0)
             return Directions.NORTH;
         else
