aae8f7e8dba595b64d8d31b3f0e69748e61a8848
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-12 03:36:26 +0200

    :cyclone: npc

--- src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
+++ src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
@@ -3,23 +3,19 @@ package fr.hardback.spigot.tools.npc;
 import net.jitse.npclib.NPCLib;
 import net.jitse.npclib.api.NPC;
 import net.jitse.npclib.api.skin.Skin;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 
 public class NPCManager{
 
-    protected final Player player;
     protected final JavaPlugin plugin;
 
     protected NPCLib npcLib;
     protected NPC npc;
 
-    public NPCManager(Player player, JavaPlugin plugin) {
-        this.player = player;
+    public NPCManager(JavaPlugin plugin) {
         this.plugin = plugin;
-
         this.npcLib = new NPCLib(this.plugin);
     }
 
@@ -28,5 +24,17 @@ public class NPCManager{
         npc.setLocation(npcList.getLocation());
         npc.setSkin(new Skin(npcList.getTexture(), npcList.getSignature()));
         npc.create();
+    }
+
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
+
+    public NPCLib getNpcLib() {
+        return npcLib;
+    }
+
+    public NPC getNpc() {
+        return npc;
     }
 }
