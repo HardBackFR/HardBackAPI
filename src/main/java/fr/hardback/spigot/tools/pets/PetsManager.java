8ea106318c8aea94930ca8836336ab329335ca6a
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-15 00:55:31 +0200

    :cyclone: pets

--- src/main/java/fr/hardback/spigot/tools/pets/PetsManager.java
+++ src/main/java/fr/hardback/spigot/tools/pets/PetsManager.java
@@ -2,6 +2,8 @@ package fr.hardback.spigot.tools.pets;
 
 import fr.hardback.spigot.tools.DirectionUtils;
 import fr.hardback.spigot.tools.head.CustomHead;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftArmorStand;
 import org.bukkit.entity.ArmorStand;
@@ -12,15 +14,19 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import xyz.xenondevs.particle.ParticleBuilder;
 import xyz.xenondevs.particle.ParticleEffect;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
 public class PetsManager {
 
-    private final ArmorStand armorStand;
+    public final Map<UUID, ArmorStand> pet;
     private final UUID uuid;
+    private final ArmorStand armorStand;
 
     public PetsManager(Player player, Pets pets){
+        this.pet = new HashMap<>();
         this.uuid = player.getUniqueId();
         this.armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
 
@@ -33,6 +39,7 @@ public class PetsManager {
         this.armorStand.setHelmet(CustomHead.create(pets.getHeadURL()));
         this.armorStand.setGravity(false);
         this.armorStand.setVisible(false);
+        this.pet.put(player.getUniqueId(), this.armorStand);
     }
 
     public void destroy(PlayerInteractAtEntityEvent event){
@@ -42,6 +49,7 @@ public class PetsManager {
             if(event.getRightClicked().getCustomName().contains(pets.getName())) {
                 event.getRightClicked().remove();
                 new ParticleBuilder(ParticleEffect.EXPLOSION_HUGE, event.getRightClicked().getLocation()).setOffsetY(1f).setSpeed(0.1f).display();
+                this.pet.remove(event.getRightClicked().getUniqueId());
             }
         }
     }
@@ -65,7 +73,7 @@ public class PetsManager {
             case SOUTH:
                 ((CraftArmorStand) armorStand).getHandle().setLocation(location.getX() - 0.5, location.getY() + 0.7, location.getZ() - 0.5, location.getYaw(), location.getPitch());
                 break;
-            default:break;
+            default: break;
         }
     }
 }
