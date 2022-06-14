c1bce2876e492156dcb696517ca282355bb5bcaf
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-14 20:42:41 +0200

    :cyclone: test

--- src/main/java/fr/hardback/spigot/tools/pets/PetManager.java
+++ src/main/java/fr/hardback/spigot/tools/pets/PetManager.java
@@ -1,13 +1,10 @@
 package fr.hardback.spigot.tools.pets;
 
-import fr.hardback.spigot.api.HardBackAPI;
 import fr.hardback.spigot.tools.head.CustomHead;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -17,26 +14,19 @@ import java.util.UUID;
 
 public class PetManager {
 
-    private final Plugin plugin;
     private final Map<UUID, Pig> petsMap =  new HashMap<>();
 
-    public PetManager(Plugin plugin) {
-        this.plugin = plugin;
-    }
-
     public void execute(Player player, Pets pets){
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            if(pets.isGround()){
-                Pig pig = (Pig) player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
+        if(pets.isGround()){
+            Pig pig = (Pig) player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
 
-                pig.setCustomName(pets.getName());
-                pig.setCustomNameVisible(true);
-                pig.getEquipment().setHelmet(CustomHead.create(pets.getHeadURL()));
-                pig.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1));
+            pig.setCustomName(pets.getName());
+            pig.setCustomNameVisible(true);
+            pig.getEquipment().setHelmet(CustomHead.create(pets.getHeadURL()));
+            pig.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1));
 
-                petsMap.put(player.getUniqueId(), pig);
-            }
-        });
+            petsMap.put(player.getUniqueId(), pig);
+        }
     }
 
     public void remove(Player player){
