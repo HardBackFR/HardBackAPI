56793ef6d45cdc80c34ccd2f2b3f39d95c898c3d
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-29 04:07:24 +0200

    :cyclone: holograms

--- src/main/java/fr/hardback/spigot/tools/holograms/HologramManager.java
+++ src/main/java/fr/hardback/spigot/tools/holograms/HologramManager.java
@@ -1,6 +1,7 @@
 package fr.hardback.spigot.tools.holograms;
 
 import eu.decentsoftware.holograms.api.DHAPI;
+import eu.decentsoftware.holograms.api.holograms.Hologram;
 import org.bukkit.Location;
 
 import java.util.List;
@@ -10,5 +11,11 @@ public class HologramManager {
 
     public static void send(Location location, List<String> lines){
         DHAPI.createHologram(UUID.randomUUID().toString(), location, true, lines);
+    }
+
+    public static void delete(){
+        for(Hologram holograms : Hologram.getCachedHolograms()){
+            holograms.delete();
+        }
     }
 }
