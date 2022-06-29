45dc8a4f57ea7419f74b202dd4a9366cf864ac80
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-29 10:58:42 +0200

    :cyclone: npc

--- src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
+++ src/main/java/fr/hardback/spigot/tools/npc/NPCManager.java
@@ -2,21 +2,22 @@ package fr.hardback.spigot.tools.npc;
 
 import dev.sergiferry.playernpc.api.NPC;
 import dev.sergiferry.playernpc.api.NPCLib;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
 import java.util.UUID;
 
 public class NPCManager {
 
-    public static void setup(Player player, Plugin plugin, NPCList npc){
-        NPC.Personal personalNPC = NPCLib.getInstance().generatePersonalNPC(player, plugin, UUID.randomUUID().toString(), npc.getLocation());
+    public static void setup(Player player, Plugin plugin, NPCList npc, boolean look){
+        NPC.Personal personalNPC = NPCLib.getInstance().generatePersonalNPC(player, plugin, npc.getName(), npc.getLocation());
         personalNPC.setSkin(new NPC.Skin(npc.getTexture(), npc.getSignature()));
         personalNPC.setItem(NPC.Slot.MAINHAND, npc.getItemStack());
         personalNPC.setText(Arrays.asList(npc.getName(), NPCList.SUB_TITLE));
-        personalNPC.setGlowing(true, ChatColor.WHITE);
         personalNPC.setCollidable(true);
         personalNPC.setFollowLookType(NPC.FollowLookType.PLAYER);
         personalNPC.setCustomTabListName(null, false);
@@ -27,5 +28,12 @@ public class NPCManager {
 
         personalNPC.create();
         personalNPC.show();
+
+        if(look){
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+                personalNPC.lookAt(player.getLocation());
+                personalNPC.update();
+            }, 0L, 20L);
+        }
     }
 }
