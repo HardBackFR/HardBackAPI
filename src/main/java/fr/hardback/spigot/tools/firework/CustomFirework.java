f3b64b35f2a563e99edd7a21046bf92631b8595b
HardBackMC <98814719+HardBackMC@users.noreply.github.com>
2022-06-29 02:20:09 +0200

    :cyclone: update

--- src/main/java/fr/hardback/spigot/tools/firework/CustomFirework.java
+++ src/main/java/fr/hardback/spigot/tools/firework/CustomFirework.java
@@ -1,19 +1,41 @@
 package fr.hardback.spigot.tools.firework;
 
-import fr.hardback.spigot.tools.color.ColorUtils;
+import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.FireworkMeta;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class CustomFirework {
 
     public static void launchFirework(Player player) {
-        Firework firework = player.getWorld().spawn(player.getEyeLocation(), Firework.class);
-        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+        final Firework firework = player.getWorld().spawn(player.getEyeLocation(), Firework.class);
+        final FireworkMeta fireworkMeta = firework.getFireworkMeta();
 
-        fireworkMeta.addEffects(FireworkEffect.builder().withColor(ColorUtils.getRandomColor()).withColor(ColorUtils.getRandomColor()).with(Type.BALL_LARGE).withFlicker().build());
+        final Color[] COLORS = new Color[]{
+                Color.AQUA,
+                Color.BLACK,
+                Color.BLUE,
+                Color.FUCHSIA,
+                Color.GRAY,
+                Color.GREEN,
+                Color.LIME,
+                Color.MAROON,
+                Color.NAVY,
+                Color.OLIVE,
+                Color.ORANGE,
+                Color.PURPLE,
+                Color.RED,
+                Color.SILVER,
+                Color.TEAL,
+                Color.WHITE,
+                Color.YELLOW
+        };
+
+        fireworkMeta.addEffects(FireworkEffect.builder().withColor(COLORS[ThreadLocalRandom.current().nextInt(COLORS.length)]).withColor(COLORS[ThreadLocalRandom.current().nextInt(COLORS.length)]).with(Type.BALL_LARGE).withFlicker().build());
         fireworkMeta.setPower(1);
         firework.setFireworkMeta(fireworkMeta);
     }
--- src/main/java/fr/hardback/spigot/tools/npc/NPCList.java
+++ src/main/java/fr/hardback/spigot/tools/npc/NPCList.java
@@ -14,7 +14,7 @@ public enum NPCList {
     KILLSKILL(ChatColor.GREEN + "KillSkill", "ewogICJ0aW1lc3RhbXAiIDogMTY1NDk3MzkxNTM0OCwKICAicHJvZmlsZUlkIiA6ICJhMGM1MjYwOWRmOTA0NzVkOTNmM2U4YWQ0YWI5Nzg2YyIsCiAgInByb2ZpbGVOYW1lIiA6ICJBbGxhaCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lODkyMTZhODI4MGM2YWY4NzViZDUyMGI3MDJjZjFkNTNlZjU1N2ZiYjI4ZWQ3OWJjZGI4MTVkZTMxYWE1NzM1IgogICAgfQogIH0KfQ==", "QhTrQLQZauSqtTuQGngCLhcvLiiJILC4bEM3IyriN8BiIN4GkY/VPiKoODLq5CbqY94/DYyQesWRs1Xov7w8eVlYt/vdXCNBwVBYR9FYaa0AfowWYWXj8yWHBW86cU4VRYxDvRH3/pzeIWtO0xStjGFG94Kzq9UdSIaruVKkpW2Bqz69pgBIPIznwbEw85U2Zr1fjxcqm+0+TCuZhYyK6EJo7r7+WCCFrIWVV42nJPWqC8eIjpqa0D4ljADrzsD/8yHJY78v7Zf1cE8IVlNVh39krsq8ZarnECGWbVGeDhE/kPOe4ifT7KYF7mpahmUBYIt043sN/FOpzkRO8ZUXkgd7uotFyNqAnSEx8nFv21kRXMENHLnFi6z0SmBxyXGMe+odkm7szEoplyL6+Wmem3FW/T6NvMNRoajuPUV+dO2DTwZXPL8s+OaZACI0fAKxn9VE/oicyA90qUOEOnRyNapYjnugDwuMVT7U6Jw1UzygN/VgdgRJpQJ1BG55yYC1lzmg/BT+wRyjQ5onRYYo4W3GllO5IlbjC8WLAlxWo4+s0o5UGlsCjrXDlTGCShWQ1zuNfA6KH9HJt1NBXGfkEfQuPsaOC9nl9L+6efMBnzAMV6oAUoQ8dmk0u1vVUT5xgv1Imqi3RDLNLlmppVpJKUrWSPcViJKCD04ryX7Mbdg=", new ItemStack(Material.GOLDEN_SWORD),new Location(Bukkit.getWorld("world"), -4.482, 100.0, 4.382, -179.8f, 1f));
 
     private final String name, texture, signature;
-    private ItemStack itemStack;
+    private final ItemStack itemStack;
     private final Location location;
 
     public static final String SUB_TITLE = ChatColor.GRAY + "CLIC-DROIT";
